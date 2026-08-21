16e5dcc5ceed86362cf2835aa15a2907212afbfb
Milox-0419 <yxy120419@gmail.com>
2026-08-21 21:17:47 +0800

    修改路径

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Sonic-Topography-Wallpaper/',
+    base: '/music/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
